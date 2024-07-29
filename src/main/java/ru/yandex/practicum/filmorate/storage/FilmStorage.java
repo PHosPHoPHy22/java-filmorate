ad538ebecf5301d749a6016a1eccbf0630871d4d
Ksenia <kseniiabelka05@mail.ru>
2024-07-29 18:54:23 +0300

    тз 11 гитхаб

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,10 +5,15 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film getFilm(int id);
+
     List<Film> getAllFilms();
+
     Film saveFilm(Film film);
+
     Film updateFilm(Film film);
+
     void deleteFilm(int id);
 
 }
